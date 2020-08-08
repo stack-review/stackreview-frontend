0b70e0773ca5c68f05f77ee104a0d9958749bbd6
Stephane Bachelier <stephane.bachelier@gmail.com>
2020-08-09 01:46:58 +0200

    refactor: migrate hook inside provider

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,19 @@
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import Header from "./Header";
 
+import { useAuth0 } from "@auth0/auth0-react";
+
 const Layout = ({ children }) => {
+  const { isLoading, error } = useAuth0();
+  
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+  
   return (
     <>
       <div>
