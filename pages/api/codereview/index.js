3db139ffcfc574afc59814e1b52a6bc429456641
Stephane Bachelier <stephane.bachelier@gmail.com>
2020-08-10 00:31:39 +0200

    feat(api/codereview): handle author for anonymous review

--- pages/api/codereview/index.js
+++ pages/api/codereview/index.js
@@ -15,6 +15,13 @@ export const config = {
   }
 }
 
+const getAuthor = (author, anonymous) => 
+  !anonymous ? author : ({
+    name: 'Anonymous',
+    sub: 'anonymous',
+    picture: 'https://ashusingh.me/assets/img/myAvatar.svg'
+  })
+
 export default async (req, res) => {
   switch (req.method) {
     case 'POST': {
@@ -28,12 +35,14 @@ export default async (req, res) => {
         return sendError(res, 400, error)
       }
 
-      const { code, language, anonymous, author } = req.body
+      const { title, description, code, language, anonymous = false, author } = req.body
 
       try {
         const collection = await getReviewCollection()
         const review = await collection.insertOne({
           rid: uuid(),
+          title,
+          description,
           code, 
           language, 
           anonymous, 
@@ -51,14 +60,20 @@ export default async (req, res) => {
     }
 
     case 'GET': {
-      if (!isReqAuthenticated(req)) {
-        return sendError(res, 401)
-      }
-
       const collection = await getReviewCollection()
-      const codereviews = await collection.find({}, { sort: { 'createdAt': -1 } }).toArray()
+      const codereviews = await collection.find({}, { 
+        sort: { 'createdAt': -1 },  
+        projection: {
+          _id: 0
+        }
+      }).toArray()
 
-      return res.status(200).json(codereviews)
+      return res.status(200).json(
+        codereviews.map(({ anonymous, author, ...rest }) => ({
+          ...rest,
+          author: getAuthor(author, anonymous)
+        }))
+      )
     }
     default:
       sendError(res, 405)
