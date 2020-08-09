50dd33b9eb1c8f2b3bcea58127f3e6ec77aeae6c
Stephane Bachelier <stephane.bachelier@gmail.com>
2020-08-09 04:10:45 +0200

    feat(api): upgrade createCodeReview service

--- pages/api/codereview/index.js
+++ pages/api/codereview/index.js
@@ -15,6 +15,7 @@ export const config = {
 export default (req, res) => {
   switch (req.method) {
     case 'POST': {
+      console.log(req.headers)
       if (!isReqAuthenticated(req)) {
         return sendError(res, 401)
       }
@@ -30,6 +31,19 @@ export default (req, res) => {
       // TODO: implement database saving
 
       return res.status(200).json({ code, language, anonymous })
+    }
+
+    case 'GET': {
+      // if (!isReqAuthenticated(req)) {
+      //   return sendError(res, 401)
+      // }
+
+      // TODO: immplement database fetching 
+
+      return res.status(200).json([
+        { code: 'function onLoad(editor) {\n  console.log("i\'ve loaded");\n}', language: 'javascript', anonymous: false },
+        { code: 'function foo () {\n  return \"bar\");\n}', language: 'javascript', anonymous: true }
+      ])
     }
     default:
       sendError(res, 405)
