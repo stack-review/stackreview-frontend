4ad0416d038dd005641c75bfdff1c739d8d3ba13
Nimai Patel <nimai.m.patel@gmail.com>
2020-08-09 10:50:57 +0530

    using html textarea

--- components/CodeDescriptionBox.jsx
+++ components/CodeDescriptionBox.jsx
@@ -2,32 +2,22 @@ import { withStyles } from "@material-ui/core/styles"
 import { TextField } from "@material-ui/core"
 import { grey } from "@material-ui/core/colors"
 
-const TextArea = withStyles({
-    root: {
-        backgroundColor: grey[300],
-        borderRadius: "10px",
-        padding: "20px",
-    },
-})(TextField)
-
 const CodeDescriptionBox = ({ description, handleDescriptionChange }) => {
     return (
-        <TextArea fullWidth placeholder="A description of your code..." InputProps={{ disableUnderline: true }} />
-
-        // <textarea
-        //     value={description}
-        //     onChange={handleDescriptionChange}
-        //     style={{
-        //         fontSize: "14px",
-        //         fontFamily: "serif",
-        //         padding: "20px",
-        //         width: "900px",
-        //         height: "300px",
-        //         resize: "none",
-        //         borderRadius: "10px",
-        //         border: "none",
-        //     }}
-        // />
+        <textarea
+            value={description}
+            onChange={handleDescriptionChange}
+            style={{
+                fontSize: "14px",
+                fontFamily: "serif",
+                padding: "20px",
+                width: "900px",
+                height: "300px",
+                resize: "none",
+                borderRadius: "10px",
+                border: "none",
+            }}
+        />
     )
 }
 
