ebba9a8f923e5820148fb965bfc026435d8d1995
ASHUTOSH KUMAR SINGH <ashusingh15673@gmail.com>
2020-08-09 15:47:11 +0530

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,9 +9,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 const Layout = ({ children }) => {
   const { isLoading, error } = useAuth0();
   
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
 
   if (error) {
     return <div>Oops... {error.message}</div>;
