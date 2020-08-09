615723052548fb3517d2519368db94414ec88ccd
Stephane Bachelier <stephane.bachelier@gmail.com>
2020-08-09 12:41:03 +0200

    feat(api/codereview): implement createCodeReview and getCodeReviewList

--- pages/api/codereview/index.js
+++ pages/api/codereview/index.js
@@ -4,6 +4,9 @@ import { isReqAuthenticated } from '@/lib/api/auth'
 import validate from '@/lib/api/validation'
 import schema from '@/lib/api/validation/createReview/schema'
 
+import { getReviewCollection } from '@/lib/db'
+import { v4 as uuid } from 'uuid'
+
 export const config = {
   api: {
     bodyParser: {
@@ -12,11 +15,10 @@ export const config = {
   }
 }
 
-export default (req, res) => {
+export default async (req, res) => {
   switch (req.method) {
     case 'POST': {
-      console.log(req.headers)
-      if (!isReqAuthenticated(req)) {
+       if (!isReqAuthenticated(req)) {
         return sendError(res, 401)
       }
 
@@ -26,24 +28,37 @@ export default (req, res) => {
         return sendError(res, 400, error)
       }
 
-      const { code, language, anonymous } = req.body
+      const { code, language, anonymous, author } = req.body
 
-      // TODO: implement database saving
+      try {
+        const collection = await getReviewCollection()
+        const review = await collection.insertOne({
+          rid: uuid(),
+          code, 
+          language, 
+          anonymous, 
+          author,
+          createdAt: Date.now()
+        })
+
+        return res.status(200).json(review)
+
+      } catch(err) {
+        console.error("Failed to save in db", err);
+      }
 
       return res.status(200).json({ code, language, anonymous })
     }
 
     case 'GET': {
-      // if (!isReqAuthenticated(req)) {
-      //   return sendError(res, 401)
-      // }
+      if (!isReqAuthenticated(req)) {
+        return sendError(res, 401)
+      }
 
-      // TODO: immplement database fetching 
+      const collection = await getReviewCollection()
+      const codereviews = await collection.find({}, { sort: { 'createdAt': -1 } }).toArray()
 
-      return res.status(200).json([
-        { code: 'function onLoad(editor) {\n  console.log("i\'ve loaded");\n}', language: 'javascript', anonymous: false },
-        { code: 'function foo () {\n  return \"bar\");\n}', language: 'javascript', anonymous: true }
-      ])
+      return res.status(200).json(codereviews)
     }
     default:
       sendError(res, 405)
